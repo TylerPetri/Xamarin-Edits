9b1398fd4b3851cd34a4758888d6c6c938973764
tyler <tyler_1701@hotmail.com>
2021-09-19 14:26:58 -0400

    initial

---
+++ src/FitnessApp/Views/ClassesPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ClassesPage : ContentPage
+    {
+        public ClassesPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Collections_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((e.CurrentSelection?.Count() ?? 0) == 0)
+                return;
+            await Shell.Current.GoToAsync($"//MainTabs/{nameof(CollectionDetailPage)}");
+            Collections.SelectedItem = null;
+        }
+    }
+}
